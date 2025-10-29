3713e91652089ac8a0887a736407f334f50f02bb
Burcu Tepekule <burcutepekule@Mac.fritz.box>
2025-10-29 12:41:53 +0100

    some changes

--- analyse_py.R
+++ analyse_py.R
@@ -2,7 +2,7 @@ library(tidyverse)
 
 # Load all files
 params    = read_csv('/Users/burcutepekule/Desktop/tregs/mass_sim_results/sampled_parameters.csv')
-results   = read_csv('/Users/burcutepekule/Desktop/tregs/mass_sim_results/all_simulation_results.csv')
+results   = read_csv('/Users/burcutepekule/Desktop/tregs/mass_sim_results/simulation_results_param_set_0.csv')
 
 # Merge
 full_data = results %>% left_join(params, by = 'param_set_id') 
