799472bec9fd96c81eda6cfbf61d7ed007c92774
Burcu Tepekule <burcutepekule@Mac.fritz.box>
2025-10-29 23:59:15 +0100

    lda plot

--- analyse_py.R
+++
@@ -1,8 +0,0 @@
-library(tidyverse)
-
-# Load all files
-params    = read_csv('/Users/burcutepekule/Desktop/tregs/mass_sim_results/sampled_parameters.csv')
-results   = read_csv('/Users/burcutepekule/Desktop/tregs/mass_sim_results/simulation_results_param_set_0.csv')
-
-# Merge
-full_data = results %>% left_join(params, by = 'param_set_id') 
